fdee8705db6b6c2d3b183567c11c3924f917359a
Keaton Caldwell <keatoncaldwell@Keatons-MacBook-Pro.local>
2025-04-23 16:18:06 -0400

    Added basic keyboard only white keys operational

--- Sounds.js
+++ Sounds.js
@@ -1,6 +1,48 @@
-// synth.js
-document.getElementById("playBtn").addEventListener("click", async () => {
-    await Tone.start();
+// synth info grabbed from Tone.js website
+
+// test button was made and given the ID playbutton
+document.getElementById("C").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
     
-    new Tone.Synth().toDestination().triggerAttackRelease("C4", "8n");
+    new Tone.Synth().toDestination().triggerAttackRelease("C4", "8n"); // copied from tone.js
+  });
+
+  document.getElementById("D").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("D4", "8n"); // copied from tone.js
+  });
+
+
+  document.getElementById("E").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("E4", "8n"); // copied from tone.js
+  });
+
+
+
+  document.getElementById("F").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("F4", "8n"); // copied from tone.js
+  });
+
+
+  document.getElementById("G").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("G4", "8n"); // copied from tone.js
+  });
+
+  document.getElementById("A").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("A4", "8n"); // copied from tone.js
+  });
+
+  document.getElementById("B").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("B4", "8n"); // copied from tone.js
   });
