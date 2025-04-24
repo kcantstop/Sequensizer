ad43514dbd3a139af447a3275a4dad5a25670bb4
Keaton Caldwell <keatoncaldwell@Keatons-MacBook-Pro.local>
2025-04-23 21:13:21 -0400

    added black key capability and started the sequencer capabilities

--- Sounds.js
+++ Sounds.js
@@ -1,7 +1,10 @@
 // synth info grabbed from Tone.js website
 
-// test button was made and given the ID playbutton
-document.getElementById("C").addEventListener("click", async () => {
+// test button was made and given the ID playbutton 
+
+// the following are white keys
+
+document.getElementById("LC").addEventListener("click", async () => {
     await Tone.start(); //copied from tone.js allows the button to execute audio
     
     new Tone.Synth().toDestination().triggerAttackRelease("C4", "8n"); // copied from tone.js
@@ -46,3 +49,45 @@ document.getElementById("C").addEventListener("click", async () => {
     
     new Tone.Synth().toDestination().triggerAttackRelease("B4", "8n"); // copied from tone.js
   });
+
+  document.getElementById("HC").addEventListener("click", async () => {
+    await Tone.start(); //copied from tone.js allows the button to execute audio
+    
+    new Tone.Synth().toDestination().triggerAttackRelease("C5", "8n"); // copied from tone.js
+  });
+
+
+  // The following are black keys
+
+  document.getElementById("C#").addEventListener("click", () => {
+    
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C#4", "8n");
+  });
+
+  document.getElementById("D#").addEventListener("click", () => {
+    
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D#4", "8n");
+  });
+
+  document.getElementById("F#").addEventListener("click", () => {
+    
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F#4", "8n");
+  });
+
+  document.getElementById("G#").addEventListener("click", () => {
+    
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("G#4", "8n");
+  });
+
+  document.getElementById("A#").addEventListener("click", () => {
+    
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A#4", "8n");
+  });
+
+
+
