9c5cec3658f185251d3ac5947dda6edc727bd937
Keaton Caldwell <keatoncaldwell@Keatons-MacBook-Pro.local>
2025-05-02 16:10:41 -0400

    fixed website layout and reduced JS code

--- Sounds.js
+++ Sounds.js
@@ -1,93 +1,32 @@
 // synth info grabbed from Tone.js website
 
-// test button was made and given the ID playbutton 
+// Piano key mapping this reduces all the extra code I had and simplfies the call back when I implement the Sequencer
+const keyMap = {
+  'LC': 'C4',
+  'D': 'D4',
+  'E': 'E4',
+  'F': 'F4',
+  'G': 'G4',
+  'A': 'A4',
+  'B': 'B4',
+  'HC': 'C5',
+  'C#': 'C#4',
+  'D#': 'D#4',
+  'F#': 'F#4',
+  'G#': 'G#4',
+  'A#': 'A#4'
+};
 
-// the following are white keys
-
-document.getElementById("LC").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("C4", "8n"); // copied from tone.js
-  });
-
-  document.getElementById("D").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("D4", "8n"); // copied from tone.js
-  });
-
-
-  document.getElementById("E").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("E4", "8n"); // copied from tone.js
-  });
+// Tone.js Setup
+const synth = new Tone.Synth().toDestination();
+let selectedNote = 'C4';
 
 
-
-  document.getElementById("F").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("F4", "8n"); // copied from tone.js
-  });
-
-
-  document.getElementById("G").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("G4", "8n"); // copied from tone.js
-  });
-
-  document.getElementById("A").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("A4", "8n"); // copied from tone.js
-  });
-
-  document.getElementById("B").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("B4", "8n"); // copied from tone.js
-  });
-
-  document.getElementById("HC").addEventListener("click", async () => {
-    await Tone.start(); //copied from tone.js allows the button to execute audio
-    
-    new Tone.Synth().toDestination().triggerAttackRelease("C5", "8n"); // copied from tone.js
-  });
-
-
-  // The following are black keys
-
-  document.getElementById("C#").addEventListener("click", () => {
-    
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("C#4", "8n");
-  });
-
-  document.getElementById("D#").addEventListener("click", () => {
-    
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("D#4", "8n");
-  });
-
-  document.getElementById("F#").addEventListener("click", () => {
-    
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("F#4", "8n");
-  });
-
-  document.getElementById("G#").addEventListener("click", () => {
-    
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("G#4", "8n");
-  });
-
-  document.getElementById("A#").addEventListener("click", () => {
-    
-    const synth = new Tone.Synth().toDestination();
-    synth.triggerAttackRelease("A#4", "8n");
-  });
-
-
-
+// Piano event listeners this is based on the original get element but uses a for each function based on the key map 
+Object.entries(keyMap).forEach(([id, note]) => {
+  document.getElementById(id).addEventListener('click', async () => {
+      await Tone.start();
+      selectedNote = note;
+      synth.triggerAttackRelease(note, '8n');
+    });
+})
